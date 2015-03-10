2e13003b0fd863196f02c63f937b8081fedd4ea2
hustlzp <hustlzp@qq.com>
2015-03-10 20:54:56 +0800

    Finish user.profile.

--- application/static/js/layout.js
+++ application/static/js/layout.js
@@ -2,6 +2,7 @@
 setTimeout(showFlash, 200);
 setTimeout(hideFlash, 2000);
 
+// 弹出用户卡片
 // TODO: Need to stay popover when hovered.
 $(document).popover({
     content: function () {
@@ -15,6 +16,44 @@ $(document).popover({
     delay: {
         'hide': 100
     }
+});
+
+// 投票
+$(document).on('click', '.vote', function () {
+    var pieceId = parseInt($(this).attr('data-piece-id')),
+        url = "",
+        votesCount = $(this).find('.votes-count').first(),
+        vote = $(this),
+        voted = $(this).hasClass('voted');
+
+    if (voted) {
+        url = urlFor('piece.unvote', {uid: pieceId});
+    } else {
+        url = urlFor('piece.vote', {uid: pieceId});
+    }
+
+    $.ajax({
+        url: url,
+        method: 'post',
+        dataType: 'json',
+        success: function (response) {
+            var currentVotesCount = parseInt(votesCount.text()),
+                targetVotesCount;
+            if (response.result) {
+                if (voted) {
+                    targetVotesCount = (currentVotesCount > 0) ? currentVotesCount - 1 : 0;
+                    $(".vote[data-piece-id=" + pieceId + "]")
+                        .removeClass('voted')
+                        .find('.votes-count').text(targetVotesCount);
+                } else {
+                    targetVotesCount = currentVotesCount + 1;
+                    $(".vote[data-piece-id=" + pieceId + "]")
+                        .addClass('voted')
+                        .find('.votes-count').text(targetVotesCount);
+                }
+            }
+        }
+    });
 });
 
 /**
