83dcdc9150964b07faa200203047a82144bcbe76
hustlzp <hustlzp@qq.com>
2015-03-21 16:47:41 +0800

    Add char size validation to Piece.content.

--- application/forms/piece.py
+++ application/forms/piece.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import re
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Email, URL, Optional
@@ -12,6 +13,16 @@ class PieceForm(Form):
     source = StringField('出处', validators=[Optional()])
     source_link = StringField('链接', validators=[Optional(), URL(message='链接格式不正确')],
                               description='选填')
+
+    def validate_content(self, field):
+        content = self.content.data
+        content = content.strip()  # 去除首尾的空格
+        content = re.sub('\s+', ' ', content)  # 将多个空格替换为单个空格
+        content = re.sub('(\r\n)+', ' ', content)  # 将换行符替换为空格
+        self.content.data = content
+
+        if len(content) > 160:
+            raise ValueError('不超过160字')
 
 
 class PieceCommentForm(Form):
