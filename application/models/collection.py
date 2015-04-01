15cd75b245c8179112f2cdee610d955a61830ba5
hustlzp <hustlzp@qq.com>
2015-04-01 19:16:48 +0800

    Update style for collection.

--- application/models/collection.py
+++ application/models/collection.py
@@ -9,7 +9,7 @@ class Collection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     created_at = db.Column(db.DateTime, default=datetime.now)
-    cover = db.Column(db.String(200))
+    cover = db.Column(db.String(200), default="default.png")
     desc = db.Column(db.Text)
 
     @property
