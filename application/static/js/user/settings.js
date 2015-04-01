77cd7a2db2a0e325c077b53a5265214236241cbe
hustlzp <hustlzp@qq.com>
2015-04-01 17:15:17 +0800

    Try to fix the path bug in plupload js.

--- application/static/js/user/settings.js
+++ application/static/js/user/settings.js
@@ -6,13 +6,21 @@
 //    }
 //}));
 
-var uploader = new plupload.Uploader({
+$.extend(g.pluploadDefaults, {
     browse_button: 'btn-upload-avatar',
     url: urlFor('user.upload_avatar'),
     multipart_params: {
         'csrf_token': g.csrfToken
     }
 });
+
+//var uploader = new plupload.Uploader({
+//    browse_button: 'btn-upload-avatar',
+//    url: urlFor('user.upload_avatar'),
+//    multipart_params: {
+//        'csrf_token': g.csrfToken
+//    }
+//});
 
 uploader.init();
 
