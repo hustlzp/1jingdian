dd1dd1b1aa29e0d8312ed4584f7e9d169c8da012
hustlzp <hustlzp@qq.com>
2015-04-04 19:03:35 +0800

    Add notification model.

---
+++ application/models/notification.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from datetime import datetime
+from ._base import db
+
+
+class NOTIFICATION_KIND(object):
+    COMMENT_PIECE = 1
+    COMMENT_PIECE_COMMENT = 2
+    COMMENT_FEEDBACK = 3
+    NEW_BLOG = 4
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    checked = db.Column(db.Boolean, nullable=False, default=False)
+    kind = db.Column(db.String(20), nullable=False)
+    target = db.Column(db.String(100))
+    content = db.Column(db.Text, default="")
+    link = db.Column(db.String(200))
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sender = db.relationship('User',
+                             backref=db.backref('sender_notifications', lazy='dynamic',
+                                                order_by="desc(Notification.created)",
+                                                cascade="all, delete, delete-orphan"),
+                             foreign_keys=[sender_id])
+
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    receiver = db.relationship('User', backref=db.backref('notifications', lazy='dynamic',
+                                                          order_by="desc(Notification.created)",
+                                                          cascade="all, delete, delete-orphan"),
+                               foreign_keys=[receiver_id])
