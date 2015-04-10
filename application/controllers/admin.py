ca04bb1762f59e8daaa76d36e31ac96c41d253cf
hustlzp <hustlzp@qq.com>
2015-04-10 11:23:45 +0800

    Now can admin feedback.

--- application/controllers/admin.py
+++ application/controllers/admin.py
@@ -1,20 +1,37 @@
 # coding: utf-8
 from flask import render_template, Blueprint, redirect, request, url_for
 from ..utils.permissions import AdminPermission
-from ..models import db, CollectionEditLogReport, PieceEditLogReport
+from ..models import db, CollectionEditLogReport, PieceEditLogReport, Feedback
 
 bp = Blueprint('admin', __name__)
 
 
-@bp.route('/report_piece_logs', methods=['GET', 'POST'])
+@bp.route('/admin/report_piece_logs', methods=['GET', 'POST'])
 @AdminPermission()
 def report_piece_logs():
     reports = PieceEditLogReport.query
     return render_template('admin/report_piece_logs.html', reports=reports)
 
 
-@bp.route('/report_collection_logs', methods=['GET', 'POST'])
+@bp.route('/admin/report_collection_logs', methods=['GET', 'POST'])
 @AdminPermission()
 def report_collection_logs():
     reports = CollectionEditLogReport.query
     return render_template('admin/report_collection_logs.html', reports=reports)
+
+
+@bp.route('/admin/feedback', methods=['GET', 'POST'])
+@AdminPermission()
+def feedback():
+    feedbacks = Feedback.query
+    return render_template('admin/feedback.html', feedbacks=feedbacks)
+
+
+@bp.route('/admin/feedback/<int:uid>/process')
+@AdminPermission()
+def process_feedback(uid):
+    feedback = Feedback.query.get_or_404(uid)
+    feedback.processed = True
+    db.session.add(feedback)
+    db.session.commit()
+    return redirect(request.referrer or url_for('.feedback'))
