5eb6b921dfd7eb22509c3861edce318a05c5f7d9
hustlzp <hustlzp@qq.com>
2015-05-07 17:20:21 +0800

    Refactor codes.

--- application/controllers/site.py
+++ application/controllers/site.py
@@ -5,7 +5,6 @@ from ..models import Piece, Collection, CollectionKind
 
 bp = Blueprint('site', __name__)
 
-
 # @bp.route('/page/<int:page>')
 # @bp.route('/', defaults={'page': 1})
 # def index(page):
@@ -23,25 +22,13 @@ bp = Blueprint('site', __name__)
 @bp.route('/')
 def index():
     """Index page."""
-    today = date.today()
-    today_pieces = Piece.get_pieces_data_by_day(today)
-    yesterday = today - timedelta(days=1)
-    yesterday_pieces = Piece.get_pieces_data_by_day(yesterday)
-    the_day_before_yesterday = today - timedelta(days=2)
-    the_day_before_yesterday_pieces = Piece.get_pieces_data_by_day(the_day_before_yesterday)
-    if today_pieces['pieces'].count():
-        first_piece = today_pieces['pieces'].first()
-    elif yesterday_pieces['pieces'].count():
-        first_piece = yesterday_pieces['pieces'].first()
-    elif the_day_before_yesterday_pieces['pieces'].count():
-        first_piece = the_day_before_yesterday_pieces['pieces'].first()
-    else:
-        first_piece = None
-    start_date = (the_day_before_yesterday - timedelta(days=1)).strftime("%Y-%m-%d")
-    return render_template('site/index.html', today_pieces=today_pieces,
-                           yesterday_pieces=yesterday_pieces,
-                           the_day_before_yesterday_pieces=the_day_before_yesterday_pieces,
-                           first_piece=first_piece, start_date=start_date)
+    pieces_data = []
+    start_date = None
+    for delta in xrange(0, 5):
+        target_day = date.today() - timedelta(days=delta)
+        pieces_data.append(Piece.get_pieces_data_by_day(target_day))
+        start_date = target_day.strftime("%Y-%m-%d")
+    return render_template('site/index.html', pieces_data=pieces_data, start_date=start_date)
 
 
 @bp.route('/about')
